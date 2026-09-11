8679a0904aa85fa25e458c955207e1b50cc90efd
Archit Awasthi <167566400+architawasthi99@users.noreply.github.com>
2026-09-11 22:04:42 +0530

    Add SQL query for product sales analysis
    Query to retrieve product names, sales year, and price from the Product and Sales tables.

---
+++ Leetcode SQL 50/07_1068. Product Sales Analysis I.sql
@@ -0,0 +1,21 @@
+/*
+Table: Sales
+
++-------------+-------+
+| Column Name | Type  |
++-------------+-------+
+| sale_id     | int   |
+| product_id  | int   |
+| year        | int   |
+| quantity    | int   |
+| price       | int   |
++-------------+-------+
+(sale_id, year) is the primary key (combination of columns with unique values) of this table.
+product_id is a foreign key (reference column) to Product table.
+Each row of this table shows a sale on the product product_id in a certain year.
+Note that the price is per unit.
+*/
+SELECT p.product_name, s.year, s.price
+FROM Product p
+JOIN Sales s
+    ON p.product_id = s.product_id;
