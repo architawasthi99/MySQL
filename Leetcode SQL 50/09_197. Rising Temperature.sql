49291739cf83928df581cc7962ccdd58597d9bed
Archit Awasthi <167566400+architawasthi99@users.noreply.github.com>
2026-09-12 22:23:10 +0530

    Create SQL query for rising temperature in Weather table
    Add SQL query to find days with rising temperatures.

---
+++ Leetcode SQL 50/09_197. Rising Temperature.sql
@@ -0,0 +1,18 @@
+/*  Table: Weather
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| recordDate    | date    |
+| temperature   | int     |
++---------------+---------+
+id is the column with unique values for this table.
+There are no different rows with the same recordDate.
+This table contains information about the temperature on a certain day. */
+
+SELECT w1.id
+FROM Weather w1
+JOIN Weather w2
+  ON DATEDIFF(w1.recordDate, w2.recordDate) = 1
+WHERE w1.temperature > w2.temperature;
