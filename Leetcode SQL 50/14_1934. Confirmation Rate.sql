82679c9aad632bc3d268bf8c5be50c5c69cc4a22
Archit Awasthi <167566400+architawasthi99@users.noreply.github.com>
2026-09-12 22:36:02 +0530

    Create 14_1934. Confirmation Rate.sql

---
+++ Leetcode SQL 50/14_1934. Confirmation Rate.sql
@@ -0,0 +1,21 @@
+/*  Table: Signups
+
++----------------+----------+
+| Column Name    | Type     |
++----------------+----------+
+| user_id        | int      |
+| time_stamp     | datetime |
++----------------+----------+
+user_id is the column of unique values for this table.
+Each row contains information about the signup time for the user with ID user_id.  */
+
+SELECT
+    s.user_id,
+    ROUND(
+        COALESCE(AVG(c.action = 'confirmed'), 0),
+        2
+    ) AS confirmation_rate
+FROM Signups s
+LEFT JOIN Confirmations c
+    ON s.user_id = c.user_id
+GROUP BY s.user_id;
