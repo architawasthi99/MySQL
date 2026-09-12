e5ec26f8e34bdc6d21cf75e3cb2422bfed44f008
Archit Awasthi <167566400+architawasthi99@users.noreply.github.com>
2026-09-12 22:17:22 +0530

    Add SQL query for managers with 5+ direct reports
    Query selects names of managers with at least five direct reports.

---
+++ 01_PRACTICE/24_570. Managers with at Least 5 Direct Reports.sql
@@ -0,0 +1,23 @@
+/* Table: Employee
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
+| department  | varchar |
+| managerId   | int     |
++-------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table indicates the name of an employee, their department, and the id of their manager.
+If managerId is null, then the employee does not have a manager.
+No employee will be the manager of themself.  */
+
+SELECT name
+FROM Employee
+WHERE id IN (
+    SELECT managerId
+    FROM Employee
+    WHERE managerId IS NOT NULL
+    GROUP BY managerId
+    HAVING COUNT(*) >= 5
+);
