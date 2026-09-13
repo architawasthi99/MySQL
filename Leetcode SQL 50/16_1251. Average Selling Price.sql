cae12189b66bdd22d4faa7e546799b48f732b8ad
Archit Awasthi <167566400+architawasthi99@users.noreply.github.com>
2026-09-13 22:56:25 +0530

    Create 16_1251. Average Selling Price.sql

---
+++ Leetcode SQL 50/16_1251. Average Selling Price.sql
@@ -0,0 +1,25 @@
+/* Table: Prices
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| product_id    | int     |
+| start_date    | date    |
+| end_date      | date    |
+| price         | int     |
++---------------+---------+
+(product_id, start_date, end_date) is the primary key (combination of columns with unique values) for this table.
+Each row of this table indicates the price of the product_id in the period from start_date to end_date.
+For each product_id there will be no two overlapping periods. That means there will be no two intersecting periods for the same product_id.*/
+
+SELECT
+    p.product_id,
+    ROUND(
+        COALESCE(SUM(p.price * u.units) / SUM(u.units), 0),
+        2
+    ) AS average_price
+FROM Prices p
+LEFT JOIN UnitsSold u
+    ON p.product_id = u.product_id
+    AND u.purchase_date BETWEEN p.start_date AND p.end_date
+GROUP BY p.product_id;
